b463d02429ce9a51ec324a8aa7728eb034d4fffa
UnkleEroshka <unkle_eroshka@mail.ru>
2016-04-18 17:42:45 +0500

    Новый консольный проект С#

---
+++ April18/April18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace April18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+        }
+    }
+}

117fa7af96085ecd67800d0d29e58411fa9bdec6
UnkleEroshka <unkle_eroshka@mail.ru>
2016-04-19 11:47:41 +0500

    Списки

--- April18/April18/Program.cs
+++ April18/April18/Program.cs
@@ -10,11 +10,17 @@ namespace April18
         static void Main(string[] args)
         {
             Point p1 = new Point(1,2,'*');
-           
-
+            Point p2 = new Point(5, 5, '#');
             p1.Draw();
-           
+            
 
+            List<char> numPoint = new List<char>();
+            numPoint.Add('#');
+            numPoint.Add('*');
+            foreach(int i in numPoint)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadLine();
         }
 

478128c4fd5280f019a93694b4a292513535dc9e
UnkleEroshka <unkle_eroshka@mail.ru>
2016-04-19 14:57:02 +0500

    new classes

---
+++ April18/April18/GorizontalLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace April18
+{
+    class GorizontalLine : Figure
+    {
+        
+        public GorizontalLine(int xLeft, int xRight,int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xLeft;x<= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+            
+
+        }
+        
+    }
+}
